1fe55bd5e5c9793c93083e0a264c3ddc874032ad
evgfomin <evgenyfomin999@gmail.com>
2026-06-13 04:43:14 +0300

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './shared/styles/reset.css';
-import './theme.css';
-import App from './App.tsx';
-import styles from './style.module.css';
-import { AuthProvider, OAuthProvider } from './features/Auth';
-import Header from './shared/ui/Header';
-import StatusBar from './shared/ui/StatusBar';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./shared/styles/reset.css";
+import "./theme.css";
+import App from "./App.tsx";
+import styles from "./style.module.css";
+import { AuthProvider, OAuthProvider } from "./features/Auth";
+import Header from "./shared/ui/Header";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <OAuthProvider>
@@ -17,7 +16,6 @@ createRoot(document.getElementById('root')!).render(
           <div className={styles.root}>
             <Header />
             <App />
-            <StatusBar />
           </div>
         </AuthProvider>
       </OAuthProvider>
